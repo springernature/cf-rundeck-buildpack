90048f8c88b8eae361af1bf63ca224b2a8e4c5de
Joscha Alisch <joscha.alisch@gmail.com>
2018-05-18 15:09:54 +0200

    Set port on startup
    Co-authored-by: Lukas Müller <lukas.mueller@springernature.com>

--- startRundeck.sh
+++ startRundeck.sh
@@ -4,10 +4,6 @@ echo "-----> Making java available"
 export PATH=$PATH:/home/vcap/app/.java/bin
 
 echo "-----> Setting rundeck-config.properties"
-echo "       server.http.port=${PORT}"
-echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./rundeck-config.properties
-echo "server.http.port=${PORT}\n" >> ./rundeck-config.properties
-
 # Replace all environment variables with syntax ${MY_ENV_VAR} with the value
 # thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
 perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./rundeck-config.properties > ./rundeck-config_replaced.properties
@@ -16,4 +12,4 @@ mv ./rundeck-config_replaced.properties ./rundeck-config.properties
 
 echo "-----> Starting Rundeck"
 export RDECK_BASE=/home/vcap/app/
-java -jar /home/vcap/app/rundeck.jar -b /home/vcap/app/ --skipinstall
+java -jar /home/vcap/app/rundeck.jar -Dhttp.server.port=${PORT} -b /home/vcap/app/ --skipinstall
