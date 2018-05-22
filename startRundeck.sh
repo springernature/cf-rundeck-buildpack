0121300375011d084b64c7aba90a20be559c95a2
Joscha Alisch <joscha.alisch@gmail.com>
2018-05-22 17:35:29 +0200

    CMSPLAT-2899: New approach - Move folder structure of user to rundeck
    Co-authored-by: Lukas Müller <lukas.mueller@springernature.com>

--- startRundeck.sh
+++ startRundeck.sh
@@ -5,32 +5,8 @@ set -e
 BASE_PATH=/home/vcap/app
 export RDECK_BASE=${BASE_PATH}/rundeck
 
-
-replaceEnvVariables () {
-    FILENAME=$1
-    TEMP_FILENAME=${FILENAME}_replaced
-
-    # Replace all environment variables with syntax ${MY_ENV_VAR} with the value
-    # thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
-    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ${FILENAME} > ${TEMP_FILENAME}
-    mv ${TEMP_FILENAME} ${FILENAME}
-}
-
 echo "-----> Making java available"
 export PATH=$PATH:${BASE_PATH}/.java/bin
-
-echo "-----> Replacing environment variables and moving property files"
-cd ${BASE_PATH}
-for FILE in ./*
-do
-    if [ -f "${FILE}" ]; then
-        echo "       replace envs in $FILE"
-        replaceEnvVariables ${FILE}
-        CONFIG_PATH=${BASE_PATH}/config/${FILE}
-        echo "       move $FILE to $CONFIG_PATH"
-        mv ${FILE} ${CONFIG_PATH}
-    fi
-done
 
 echo "-----> Starting Rundeck"
 
@@ -42,7 +18,7 @@ JAVA_CALL="${ADDITIONAL_ARGS} \
     -jar $RDECK_BASE/rundeck.jar \
     -b $RDECK_BASE \
     --skipinstall \
-    -c "${BASE_PATH}/config" -d"
+    -d"
 
 echo "       execute 'java $JAVA_CALL'"
 java ${JAVA_CALL}
