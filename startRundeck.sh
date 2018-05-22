eb649e3ae22ef448f211b69e7cd6183f4de457e4
Joscha Alisch <joscha.alisch@gmail.com>
2018-05-22 14:40:37 +0200

    CMSPLAT-2899: Build rundeck into ./rundeck use base as config dir
    Co-authored-by: Lukas Müller <lukas.mueller@springernature.com>

--- startRundeck.sh
+++ startRundeck.sh
@@ -1,35 +1,31 @@
 #!/bin/sh
 
-BASE_PATH=/home/vcap/app/
+BASE_PATH=/home/vcap/app
 
 echo "-----> Making java available"
 export PATH=$PATH:${BASE_PATH}.java/bin
 
-echo "-----> Setting rundeck-config.properties"
-# Replace all environment variables with syntax ${MY_ENV_VAR} with the value
-# thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./rundeck-config.properties > ./rundeck-config_replaced.properties
-mv ./rundeck-config_replaced.properties ./rundeck-config.properties
-
+echo "-----> Replacing environment variables"
+for file in ${BASE_PATH}/*
+do
+    if [[ -f ${file} ]]; then
+        echo "       in $file"
+        ./replaceEnvVariables.sh ${file}
+    fi
+done
 
 echo "-----> Starting Rundeck"
-export RDECK_BASE=${BASE_PATH}
+export RDECK_BASE=${BASE_PATH}/rundeck
 
 ADDITIONAL_ARGS="-Dserver.http.port=${PORT}"
-
-if [ -f ${BASE_PATH}jaas-login.conf ]; then
-    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./jaas-login.conf > ./jaas-login_replaced.conf
-    mv ./jaas-login_replaced.conf ./jaas-login.conf
-    ADDITIONAL_ARGS="${ADDITIONAL_ARGS} -Dloginmodule.conf.name=jaas-login.conf -Dloginmodule.name=login"
-fi
-
 
 ADDITIONAL_ARGS="${ADDITIONAL_ARGS} -Dloginmodule.conf.name=jaas-login.conf -Dloginmodule.name=RDpropertyfilelogin"
 
 JAVA_CALL="${ADDITIONAL_ARGS} \
-    -jar /home/vcap/app/rundeck.jar \
-    -b ${BASE_PATH} \
-    --skipinstall"
+    -jar $RDECK_BASE/rundeck.jar \
+    -b $RDECK_BASE \
+    --skipinstall \
+    --configdir '${BASE_PATH}'"
 
 echo "       execute 'java $JAVA_CALL'"
 java ${JAVA_CALL}
