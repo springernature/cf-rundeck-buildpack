9c5fc29e0846b4b8087358d76cd0b5bf9df15295
Admir Memic <admir.memic@springer.com>
2022-08-30 10:39:29 +0200

    Change RUNDECK_DIST_URL
    Bintray was decommissioned.

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
-export RUNDECK_DIST_URL="http://dl.bintray.com/rundeck/rundeck-maven/"
+export RUNDECK_DIST_URL="https://download.rundeck.org/war/"
 export RUNDECK_WAR="rundeck-${RUNDECK_VERSION}.war"
